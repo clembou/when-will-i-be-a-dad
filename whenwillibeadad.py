d404519b5ecf26ea966a62337d2e9a3734db232a
joekane3 <joseph.kane@res-ltd.com>
2016-07-13 15:58:31 +0200

    graph of parenthood probability.

--- whenwillibeadad.py
+++ whenwillibeadad.py
@@ -26,6 +26,24 @@ def todays_prob(due_date = dt.datetime(2016,7,21,12)):
 
     return cond_prob
 
-if __name__ == "main":
-    y = todays_prob()
+def cumulative_graph(due_date = dt.datetime(2016,7,21,12)):
+    from flask import render_template
+    df = pd.read_csv("probs.csv")
+    today = dt.datetime.now()
+    days_to_due = int((today - due_date).total_seconds()/3600.0/24.0)
+    df['Date'] = df['Day relative to due date'].apply( lambda d : dt.timedelta(days=d)) + due_date
+    df['Date'] = df['Date'].dt.strftime('"%Y-%m-%d, ')
+    df["Cumulative"] = df["Prob"].cumsum()
+    df['newline'] = u'\n" + '
+
+    with open('static/graph.html') as f:
+        s = f.read()
+        s = s.replace("{data}",df[['Date','Cumulative','newline']].to_string(header=False,index=False))
+
+    return s
+
+
+
+if __name__ == "__main__":
+    y = cumulative_graph()
     print("There is a %.1f%% chance you will be a dad today" % y)

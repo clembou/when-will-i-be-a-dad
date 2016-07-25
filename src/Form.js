80d0e8d5b133b61016dbe0ddb0a967ca520924bc
Clément Bouscasse <jahfet@gmail.com>
2016-07-25 22:34:01 +0100

    Improve form and temporarily switch to html5 urls

--- src/Form.js
+++ src/Form.js
@@ -1,45 +1,55 @@
 import * as React from 'react';
-import { FormGroup, ControlLabel, FormControl, HelpBlock } from 'react-bootstrap';
+import { FormGroup, ControlLabel, FormControl, HelpBlock, Button } from 'react-bootstrap';
 import DateTimeField from 'react-bootstrap-datetimepicker';
+import { browserHistory } from 'react-router';
+import 'react-bootstrap-datetimepicker/css/bootstrap-datetimepicker.css';
 
 export class Form extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      value: '',
+      futureDad: '',
+      dueDate: '',
     };
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  getValidationState() {
-    const length = this.state.value.length;
-    if (length > 10) return 'success';
-    else if (length > 5) return 'warning';
-    else if (length > 0) return 'error';
+  handleNameChange(e) {
+    this.setState({ futureDad: e.target.value });
   }
 
-  handleChange(e) {
-    this.setState({ value: e.target.value });
+  handleDateChange(e) {
+    console.log(e);
+    this.setState({ dueDate: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    browserHistory.push(`/${this.state.futureDad}/${this.state.dueDate}/`);
   }
 
   render() {
     return (
       <form>
-        <FormGroup
-          controlId="wwibadForm"
-          validationState={this.getValidationState()}
-        >
+        <FormGroup controlId="wwibadForm">
           <ControlLabel>What is your name?</ControlLabel>
           <FormControl
             type="text"
             value={this.state.value}
             placeholder="Enter your name"
-            onChange={this.handleChange}
-          />
+            onChange={this.handleNameChange}
+            />
           <FormControl.Feedback />
           <HelpBlock>Validation is based on string length.</HelpBlock>
 
-          <DateTimeField mode="date" />
+          <DateTimeField mode="date" onChange={this.handleNameChange} inputFormat="DD-MM-YYYY" />
         </FormGroup>
+        <Button type="submit" onClick={this.handleSubmit}>
+          Submit
+        </Button>
       </form>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import ResultPage from './ResultPage';
 import HomePage from './HomePage';
-import { Router, Route, hashHistory } from 'react-router';
-
+import { Router, Route, browserHistory } from 'react-router';
 
 ReactDOM.render((
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={HomePage}>
-      <Route path="/:futureDad/:dueDate" component={ResultPage} />
+      <Route path="/:futureDad/:dueDate/" component={ResultPage} />
       <Route path="*" component={HomePage} />
     </Route>
   </Router>),

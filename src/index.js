8503dea5d4613b89e0c871eddaa1fb33664e0e92
Clément Bouscasse <jahfet@gmail.com>
2016-09-11 17:09:01 +0100

    Update packages
    - add moment
    - Add react-datetime
    - remove react-bootstrap-datetimepicker
    - use css imports instead of hard coded links in the html

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import * as ReactDOM from 'react-dom';
 import ResultPage from './ResultPage';
 import HomePage from './HomePage';
 import { Router, Route, hashHistory } from 'react-router';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-datetime/css/react-datetime.css';
 
 ReactDOM.render((
   <Router history={hashHistory}>
